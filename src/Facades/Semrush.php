5af559f6dec0a8d7962a8dd83ae2ed4fdbb27707
Mike Vermeer <77960135+MikeCVermeer@users.noreply.github.com>
2026-02-10 15:51:30 +0100

    Add Semrush facade and README docs
    - add Semrush facade for connector access via Facade API
    
    - document Semrush env variable, connector usage, facade usage, and integration list entry

---
+++ src/Facades/Semrush.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Seeders\ExternalApis\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use Seeders\ExternalApis\Connectors\Semrush\SemrushConnector;
+
+/**
+ * @method static \Saloon\Http\Response send(\Saloon\Http\Request $request, \Saloon\Http\Faking\MockClient|null $mockClient = null, callable|null $handleRetry = null)
+ *
+ * @see \Seeders\ExternalApis\Connectors\Semrush\SemrushConnector
+ */
+final class Semrush extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return SemrushConnector::class;
+    }
+}
